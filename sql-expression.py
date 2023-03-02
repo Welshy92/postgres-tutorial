db23beb0cf00ebf7602e51c9350ab61f5bc78365
Jacob Welsh <welshy92@outlook.com>
2023-03-02 17:30:13 +0000

    Add further queries

--- sql-expression.py
+++ sql-expression.py
@@ -10,26 +10,26 @@ meta = MetaData(db)
 # Create variable for "Artist" table.
 artist_table = Table(
     "Artist", meta,
-    Column("ArtistID", Integer, primary_key=True),
+    Column("ArtistId", Integer, primary_key=True),
     Column("Name", String)
 )
 
 # Create variable for "Album" table.
 album_table = Table(
     "Album", meta,
-    Column("AlbumID", Integer, primary_key=True),
+    Column("AlbumId", Integer, primary_key=True),
     Column("Title", String),
-    Column("ArtistID", Integer, ForeignKey("artist_table.ArtistID"))
+    Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
 )
 
 # Create variable for "Track" table.
 track_table = Table(
     "Track", meta,
-    Column("TrackID", Integer, primary_key=True),
+    Column("TrackId", Integer, primary_key=True),
     Column("Name", String),
-    Column("ArtistID", Integer, ForeignKey("album_table.ArtistID")),
-    Column("MediaTypeID", Integer, primary_key=False),
-    Column("GenreID", Integer, primary_key=False),
+    Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
+    Column("MediaTypeId", Integer, primary_key=False),
+    Column("GenreId", Integer, primary_key=False),
     Column("Composer", String),
     Column("Milliseconds", Integer),
     Column("Bytes", Integer),
@@ -40,8 +40,23 @@ track_table = Table(
 # Make the connection
 with db.connect() as connection:
 
-    # Query 1 Select all from the artists table
-    select_query = artist_table.select()
+    # Query 1 Select all from the artist table
+    # select_query = artist_table.select()
+
+    # Query 2 Select names from the artist table
+    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+
+    # Query 3 Find the artist name Queen only
+    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+
+    # Query 4 Find the artistID of 51
+    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    # Query 5 Select albums with the aritstID of 51
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+    # Query 6
+    select_query = track_table.select().where(track_table.c.Composer == "Queen")
 
     results = connection.execute(select_query)
     for result in results:
