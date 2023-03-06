2b810db9d66b26b9540c7ed6a6cd83214e4ae31b
Jacob Welsh <welshy92@outlook.com>
2023-03-06 13:36:02 +0000

    add famous programmers to database

--- sql-crud.py
+++ sql-crud.py
@@ -38,3 +38,75 @@ ada_lovelace = Programmer(
     nationality="Brtish",
     famous_for="First Programmer"
 )
+
+alan_turing = Programmer(
+    first_name="Alan",
+    last_name="Turing",
+    gender="M",
+    nationality="British",
+    famous_for="Modern Computing"
+)
+
+grace_hopper = Programmer(
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL language"
+)
+
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11"
+)
+
+bill_gates = Programmer(
+    first_name="Bill",
+    last_name="Gates",
+    gender="M",
+    nationality="American",
+    famous_for="Microsoft"
+)
+
+tim_berners_lee = Programmer(
+    first_name="Tim",
+    last_name="Berners-Lee",
+    gender="M",
+    nationality="British",
+    famous_for="World Wide Web"
+)
+
+jacob_welsh = Programmer(
+    first_name="Jacob",
+    last_name="Welsh",
+    gender="M",
+    nationality="British",
+    famous_for="Code Institute"
+)
+
+# add each instance of our programmers to our session
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+# session.add(jacob_welsh)
+
+# commit our session to the database
+# session.commit()
+
+# find all programmers in the database
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep=" | "
+        )
